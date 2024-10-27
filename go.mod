db11126e61799d0c72c326679dddddccb62f9ef1
Yegor Kazantsev <wwwadmin@bk.ru>
2024-10-27 04:59:11 +0300

    tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/saintbyte/postgresURItoDSN
+
+go 1.22.2
---
+++ uri2dsn.go
@@ -0,0 +1,6 @@
+package postgresURItoDSN
+
+func URItoDSN(URI string) (string, error) {
+	//postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...]
+	return "", nil
+}
